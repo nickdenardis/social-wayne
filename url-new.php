81c5d32452868a6f4da965bb9c821657a8971442
Nick DeNardis <nick.denardis@gmail.com>
2012-10-02 09:14:30 -0400

    Added some base level stuff to get the URL's created.

--- url-new.php
+++ url-new.php
@@ -4,6 +4,55 @@
 	
 	$page_title = 'New URL';
 	
+	// If creating a new URL
+	if (isset($_POST['submit'])){
+		// Make sure this is a valid URL
+		$url_valid = false;
+		
+		if (substr(trim($_POST['url']), 0, 4) == 'http'){
+			// Use CURL
+			include_once(ROOT . '/lib/curl/curl.php');
+			$myCurl = new cURL;
+			
+			// Grab the homepage to get all the info about it
+			$page = $myCurl->get(trim($_POST['url']), NULL, true);
+			
+			 // If the page is returned successfully
+	        if ($page['info']['http_code'] == '200'){
+				$page_title = 'Page: ' . $page['info']['url'];
+			
+				// Include the DOM functions
+				include_once(ROOT . '/lib/dom/simple_html_dom.php');
+				
+				// Grab the page title
+				$html = str_get_html($page['response']);
+				if (is_object($html)){
+					$page_title = trim($html->find('title', 0)->plaintext);
+				}
+			}
+			
+			// Setup the URL Params
+			$url_params['url'] = $page['info']['url'];
+			$url_params['title'] = $page_title;
+			
+			$url_valid = true;
+		}
+		
+		if ($url_valid){
+			// Setup the social vars
+			$url_params['utm_source'] = 'twitter';
+			$url_params['utm_medium'] = 'go.wayne.edu';
+			$url_params['utm_campaign'] = 'social';
+			$url_params['utm_content'] = '';
+			
+			$url_response = $c->sendRequest('go/url/create', $url_params , 'post', true);
+			
+			Pre($url_response);
+		}else{
+			Flash('The URL you entered is not valid, please try again', 'error');
+		}
+	}
+	
 	include_once(ROOT . '/_header.php');
 ?>
 <div class="row-fluid">
@@ -24,8 +73,23 @@
 	
 	<div class="span6">
 		<h1>Add New URL</h1>
-		
-		
+		<form method="post">
+		<div class="controls">
+		  <label>URL</label>
+		  <input type="text" placeholder="http://..."  class="input-xxlarge" name="url">
+		</div>
+		  <input type="submit" class="btn" name="submit" value="Create" />
+		</form>
+		<!-- 
+		utm_source
+		utm_medium
+		utm_campaign
+		utm_content
+		utm_term
+		url
+		title
+		short_url
+		-->
 	</div>
 </div>
 
