fd681a5bccb645ec4c0673ccf2cb3571578f37d8
Nick DeNardis <nick.denardis@gmail.com>
2012-11-19 15:43:39 -0500

    Officially changed the name to Socily

--- _header.php
+++ _header.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title><?php echo h($page_title); ?> - Social Dashboard - Wayne State University</title>
+        <title><?php echo h($page_title); ?> - Socialy - Wayne State University</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
